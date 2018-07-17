74b9f54ac1f97a3f846e48f9c25dadba5a29620c
nickbeam <dvininnick@gmail.com>
2018-07-17 13:29:18 +0300

    MainReflection collapse catch

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -5,7 +5,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws IllegalAccessException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
