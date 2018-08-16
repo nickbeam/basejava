8fdbf1b9c38e4785a1e8ff44dbb1debc7a60f269
nick <dvininnick@gmail.com>
2018-08-16 20:24:32 +0300

    HW6, JunitTestSuite

--- test/ru/javaops/webapp/storage/JunitTestSuite.java
+++ test/ru/javaops/webapp/storage/JunitTestSuite.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class, SortedArrayStorageTest.class, ListStorageTest.class, MapUuidStorageTest.class, MapFullNameStorageTest.class})
+@Suite.SuiteClasses({ArrayStorageTest.class, SortedArrayStorageTest.class, ListStorageTest.class, MapUuidStorageTest.class, MapResumeStorageTest.class})
 public class JunitTestSuite {
 }
--- test/ru/javaops/webapp/storage/MapResumeStorageTest.java
+++ test/ru/javaops/webapp/storage/MapResumeStorageTest.java
@@ -1,9 +1,5 @@
 package ru.javaops.webapp.storage;
 
-import org.junit.Assert;
-import org.junit.Test;
-import ru.javaops.webapp.model.Resume;
-
 public class MapResumeStorageTest extends AbstractStorageTest{
     public MapResumeStorageTest() {
         super(new MapResumeStorage());
