de5fb10971ffe19b5af30375ef7def7199f9f969
nickbeam <dvininnick@gmail.com>
2018-09-07 18:33:16 +0300

    HW8, Add Position class in Organisation

--- src/ru/javaops/webapp/model/Organisation.java
+++ src/ru/javaops/webapp/model/Organisation.java
@@ -1,55 +1,49 @@
 package ru.javaops.webapp.model;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+
+import static ru.javaops.webapp.util.DateUtil.of;
 
 public class Organisation {
     private final String name;
     private final String url;
-    private final LocalDate startDate;
-    private final LocalDate endDate;
-    private final String head;
-    private final String description;
+    private final List<Position> positions;
 
-    public Organisation(String name, String url, LocalDate startDate, LocalDate endDate, String head, String description) {
+    public Organisation(String name, String url, Position... positions){
         Objects.requireNonNull(name, "name can't be NULL");
-        Objects.requireNonNull(startDate, "startDate can't be NULL");
-        Objects.requireNonNull(head, "head can't be NULL");
         this.name = name;
         this.url = url;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.head = head;
-        this.description = description;
+        this.positions = Arrays.asList(positions);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Organisation that = (Organisation) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(url, that.url) &&
-                Objects.equals(startDate, that.startDate) &&
-                Objects.equals(endDate, that.endDate) &&
-                Objects.equals(head, that.head) &&
-                Objects.equals(description, that.description);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, url, startDate, endDate, head, description);
-    }
 
-    @Override
-    public String toString() {
-        return "Organisation{" +
-                "name='" + name + '\'' +
-                ", url='" + url + '\'' +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", head='" + head + '\'' +
-                ", description='" + description + '\'' +
-                '}';
+    public class Position{
+        private final LocalDate startDate;
+        private final LocalDate endDate;
+        private final String head;
+        private final String description;
+
+        public Position(int year, Month month, String head, String description) {
+            this(of(year, month), LocalDate.now(), head, description);
+        }
+
+        public Position(LocalDate startDate, LocalDate endDate, String head, String description) {
+            this(startDate, endDate, head, description);
+        }
+
+        public Position(LocalDate startDate, LocalDate endDate, String head, String description) {
+            Objects.requireNonNull(startDate, "startDate can't be NULL");
+            Objects.requireNonNull(head, "head can't be NULL");
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.head = head;
+            this.description = description;
+        }
+
     }
 }
