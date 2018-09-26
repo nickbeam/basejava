7247da8bfe4f58b62b9f155fa605793cd626c0eb
nick <dvininnick@gmail.com>
2018-09-26 07:48:48 +0300

    HW9, PathStorage implementation

---
+++ src/ru/javaops/webapp/storage/PathStorage.java
@@ -0,0 +1,109 @@
+package ru.javaops.webapp.storage;
+
+import ru.javaops.webapp.exception.StorageException;
+import ru.javaops.webapp.model.Resume;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class PathStorage extends AbstractStorage<Path> {
+    private Path directory;
+
+//    protected abstract Resume doRead(InputStream inputStream) throws IOException;
+//
+//    protected abstract void doWrite(Resume resume, OutputStream outputStream) throws IOException;
+
+    protected PathStorage(String dir) {
+        directory = Paths.get(dir);
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!Files.isDirectory(directory)) {
+            throw new IllegalArgumentException(dir + " is not directory");
+        }
+        if (!Files.isReadable(directory) || !Files.isWritable(directory)) {
+            throw new IllegalArgumentException(dir + " is not readable/writable");
+        }
+    }
+
+    @Override
+    protected void doSave(Resume resume, Path path) {
+        try {
+            Files.createFile(path);
+            doUpdate(resume, path);
+        } catch (IOException e) {
+            throw new StorageException("File create error " + path.toString(), path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected Path getSearchKey(String uuid) {
+        return directory.resolve(uuid);
+    }
+
+    @Override
+    protected Resume doGet(Path path) {
+        try {
+            return doRead(new BufferedInputStream(new FileInputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("File read error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, Path path) {
+        try {
+            doWrite(resume, new BufferedOutputStream(new FileOutputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("File write error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("File delete error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected boolean isExist(Path path) {
+        return Files.exists(path);
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        List<Resume> resumes = new ArrayList<>();
+        try {
+            Files.list(directory)
+                    .forEach(file -> resumes.add(doGet(file.toAbsolutePath())));
+        } catch (IOException e) {
+            throw new IllegalArgumentException(directory.toString() + " is not directory", e);
+        }
+        return resumes;
+    }
+
+    @Override
+    public void clear() {
+        try {
+            Files.list(directory).forEach(this::doDelete);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", null);
+        }
+    }
+
+    @Override
+    public int size() {
+        try {
+            return Math.toIntExact(Files.list(directory).count());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
