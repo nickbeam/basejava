cc3f7f1c7769705229426f43c455cb89e3dc9fe1
Nikolai Dvinin <dvininnick@gmail.com>
2018-11-11 22:19:28 +0300

    HW12, refactoring

--- src/ru/javaops/webapp/MainStreams.java
+++ src/ru/javaops/webapp/MainStreams.java
@@ -6,24 +6,34 @@ import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
-        System.out.println(minValue(new int[]{9 , 8}));
+        System.out.println(minValue(new int[]{9, 8}));
         System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 3, 2, 3)));
     }
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values)
+        return (int) Arrays.stream(values)
                 .distinct()
                 .sorted()
-                .reduce((s1, s2) -> Integer.parseInt(String.valueOf(s1) + String.valueOf(s2)))
+                .mapToDouble(num -> (double) num)
+                .reduce((s1, s2) -> (s1 + (s2 / 10)) * 10)
                 .orElse(0);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        long sum = integers.stream().mapToInt(Integer::intValue).sum();
-        if (sum % 2 == 0) {
-            return integers.stream().filter((s) -> s % 2 != 0).collect(Collectors.toList());
-        } else {
-            return integers.stream().filter((s) -> s % 2 == 0).collect(Collectors.toList());
-        }
+
+
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
+        return integers.stream().filter((s) -> {
+            if (sum % 2 == 0 && s % 2 == 0) {
+                return false;
+            } else return sum % 2 == 0 || s % 2 == 0;
+        }).collect(Collectors.toList());
+
+//        if (sum % 2 == 0) {
+//            return integers.stream().filter((s) -> s % 2 != 0).collect(Collectors.toList());
+//        } else {
+//            return integers.stream().filter((s) -> s % 2 == 0).collect(Collectors.toList());
+//        }
     }
 }
