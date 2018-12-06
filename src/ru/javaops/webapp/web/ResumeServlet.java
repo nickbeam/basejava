8930ad0391f6bbbb3efc83081282ca3779e4690d
nickbeam <dvininnick@gmail.com>
2018-12-06 14:04:01 +0300

    HW15, ResumeServlet refactoring

--- src/ru/javaops/webapp/web/ResumeServlet.java
+++ src/ru/javaops/webapp/web/ResumeServlet.java
@@ -1,13 +1,22 @@
 package ru.javaops.webapp.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ResumeServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+public class ResumeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null) {
+            response.getWriter().write("Hello Resumes!");
+        } else {
+            response.getWriter().write("Hello " + name + "!");
+        }
     }
 }
